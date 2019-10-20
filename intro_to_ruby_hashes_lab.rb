2dc2b5e629eb1ddb4585c1ecd6cd93cde8d7db46
Vivian Tran <viviantran94@gmail.com>
2019-10-20 10:06:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ end
 def monopoly_with_second_tier
    new_hash = {
     :railroads => {
-      :pieces => 4
+      :pieces => 4,
       :rent_in_dollars => {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
     }
   }
